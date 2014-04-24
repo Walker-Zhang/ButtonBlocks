2b05965adc7f383807addb89f5086d3292a42db9
qianfeng <apple@aofel.local>
2014-04-24 15:00:57 +0800

    Initial Commit

---
+++ ButtonBlocksTests/ButtonBlocksTests.m
@@ -0,0 +1,34 @@
+//
+//  ButtonBlocksTests.m
+//  ButtonBlocksTests
+//
+//  Created by qianfeng on 14-4-24.
+//  Copyright (c) 2014年 aofel. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ButtonBlocksTests : XCTestCase
+
+@end
+
+@implementation ButtonBlocksTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
